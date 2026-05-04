ba27a39f095b3b92f4b0e437627f18399387987e
aaravparikh86-beep <aaravparikh86@gmail.com>
2026-05-04 21:14:51 +0530

    feature/upadate branch added

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
+my_dict = {"Police" : 100 , "Ambulance" : 108, "Fire" : 101}
 add_contacts = "add contacts"
 search_contacts = "search contacts"
 delete_contacts = "delete contacts"
 view_contacts = "view contacts"
 exit_app = "exit app"
 
+print("Welcome to contact book app")
 a = 1
 while a > 0:
-    print("Welcome to contact book app")
     print("what do you want to do:-")
     print("add contacts")
     print("search contacts")
@@ -17,21 +18,45 @@ while a > 0:
     Input = input("")
     Input_1 = Input.strip()
     Input_1 = Input_1.lower()
-    if Input_1 in add_contacts:
-        add = int(input("How many contacts do you want to add: "))
-        add +=1
-        b = 1
-        while b < add:
-            contacts = input(f"Contact {b}: ")
-            contact_no = input(f"Contact number {b}: ")
-            contact_no_1 = contact_no.strip()
-            contact_no_2 = len(contact_no_1)
-            if contact_no_2 == 10:
-                pass
-            else:
-                print("Enter a proper contact number")
-            if contact_no_1.isdigit():
-                pass
-            else:
-                print("Enter a 10-digit number")
+
+
+def add_contacts():
+        if Input_1 in add_contacts:
+            add = int(input("How many contacts do you want to add: "))
+            add +=1
+            b = 1
+            while b < add:
+                contacts = input(f"Contact {b}: ")
+                contact_no = int(input(f"Contact number {b}: "))
+                contact_no_1 = contact_no
+                contact_no_2 = len(str(contact_no_1))
+                if contact_no_2 == 10:
+                    if str(contact_no_1).isnumeric():
+                        if contacts not in my_dict.keys():
+                            if contact_no_1 not in my_dict.values():
+                                my_dict[contacts] = contact_no_1
+                                pass
+                            else:
+                                contact_no_3 = input("Do you want to update the contact")
+                        else:
+                            print("You already have saved a contact by this name")
+                            contacts_1 = input("Do you want to change the name")
+                            contacts_1_1 = contacts_1.lower().strip()
+                            if contacts_1 == "yes":
+                                contacts_2 = input("Enter the correct name")
+                                if contacts_2 not in my_dict.keys():
+                                    my_dict[contacts_2] = contact_no_1
+                                    pass
+                                else:
+                                    print("You already have saved a contact by this name")
+                                    break
+                    else:
+                        print("Enter a 10-digit number")
+                        break
+                else:
+                    print("Enter a proper contact number")
+                    break
+
             b+=1
+
+add_contacts()
